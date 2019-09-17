e6e76af6154d31162f8d1c1cf1e6da10d98b1fd1
Grégory LE GAREC <gregory.legarec@gmail.com>
2019-09-18 00:00:48 +0200

    feat: Configure rematch/loading 📝
    rematch/loading will help us knowing about the loading
    state of our models and effects.

--- src/store.js
+++ src/store.js
@@ -1,8 +1,11 @@
 import { init } from "@rematch/core";
+import createLoadingPlugin from '@rematch/loading'
+
 import * as models from "./models";
 
 const store = init({
-  models
+  models,
+  plugins: [createLoadingPlugin({})]
 });
 
 export default store;
