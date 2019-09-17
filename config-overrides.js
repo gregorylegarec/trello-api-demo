7ee21fdb5f759102da1f8f5d18e28e144f3f2cfb
Grégory LE GAREC <gregory.legarec@gmail.com>
2019-09-18 00:03:20 +0200

    feat: Configure react-app-rewired 📝
    react-app-rewired allows us to override the default
    webpack configuration from react-scripts.
    
    We can then define an external directive for the Trello
    client.

---
+++ config-overrides.js
@@ -0,0 +1,11 @@
+/* config-overrides.js */
+
+module.exports = function override(config, env) {
+  //do stuff with the webpack config...
+  return {
+    ...config,
+    externals: {
+      trello: "Trello"
+    }
+  };
+};
